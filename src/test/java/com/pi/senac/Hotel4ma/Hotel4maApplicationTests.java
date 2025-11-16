f39a092a9ef5a1270b4a1f7c36a0daf0f8433566
ErickGX <ericksaopaulino1@hotmail.com>
2025-11-16 20:40:12 -0300

    Correção e configuração correta do ambiente de dados , utilizando banco h2 em memoria proprio , configs no applications-test.yml

--- src/test/java/com/pi/senac/Hotel4ma/Hotel4maApplicationTests.java
+++ src/test/java/com/pi/senac/Hotel4ma/Hotel4maApplicationTests.java
@@ -2,8 +2,10 @@ package com.pi.senac.Hotel4ma;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Hotel4maApplicationTests {
 
 	@Test
